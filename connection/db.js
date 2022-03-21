0dd05f040808141943b6093881854967ac3d98e7
hasan <hsanromadon@gmail.com>
2022-03-21 23:08:21 +0700

    final project

--- connection/db.js
+++ connection/db.js
@@ -3,7 +3,7 @@ const { Pool } = require('pg');
 const dbPool = new Pool({
     host: "ec2-44-192-245-97.compute-1.amazonaws.com",
     user: "ullvmhlbycfuzx",
-    port: 5432,
+    port: "5432",
     password: "99fe7c057106d683db7c3fc935684f756f123680db54798f217e5786c11321f9",
     database: "dbvf5314ua3tbq"
 });
