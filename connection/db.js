68deb4352c21d8543579e265a28dd258b4750e38
hasan <hsanromadon@gmail.com>
2022-03-21 23:01:49 +0700

    config databse

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,21 @@
 const { Pool } = require('pg');
 
 const dbPool = new Pool({
-    host: "localhost",
-    user: "postgres",
+    host: "ec2-44-192-245-97.compute-1.amazonaws.com",
+    user: "ullvmhlbycfuzx",
     port: 5432,
-    password: "123",
-    database: "postgres"
+    password: "99fe7c057106d683db7c3fc935684f756f123680db54798f217e5786c11321f9",
+    database: "dbvf5314ua3tbq"
 });
 
+// const dbPool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     port: 5432,
+//     password: "123",
+//     database: "postgres"
+// });
+
 module.exports = dbPool;
+
+// postgres://ullvmhlbycfuzx:99fe7c057106d683db7c3fc935684f756f123680db54798f217e5786c11321f9@ec2-44-192-245-97.compute-1.amazonaws.com:5432/dbvf5314ua3tbq
