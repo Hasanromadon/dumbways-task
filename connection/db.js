3e7541c0a06ab5c7319ac40a384f9071ee432e9c
hasan <hsanromadon@gmail.com>
2022-03-22 01:15:05 +0700

    final

--- connection/db.js
+++ connection/db.js
@@ -1,11 +1,11 @@
 const { Pool, } = require('pg');
 
 const dbPool = new Pool({
-    host: "ec2-44-192-245-97.compute-1.amazonaws.com",
-    user: "ullvmhlbycfuzx",
+    host: "ec2-34-238-37-113.compute-1.amazonaws.com",
+    user: "stupvablyvfhuo",
     port: "5432",
-    password: "99fe7c057106d683db7c3fc935684f756f123680db54798f217e5786c11321f9",
-    database: "dbvf5314ua3tbq",
+    password: "cc1be3edc71e6781920903cc42a832de9c50cdda9fd652ade1b920fcc3a1378e",
+    database: "d74494t4318bbn",
     ssl: { rejectUnauthorized: false }
 });
 
