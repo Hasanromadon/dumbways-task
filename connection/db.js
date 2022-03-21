f3b53e8fab34de92c4b0796c33b3e604d801666c
hasan <hsanromadon@gmail.com>
2022-03-21 23:13:40 +0700

    final

--- connection/db.js
+++ connection/db.js
@@ -1,12 +1,11 @@
-const { Pool } = require('pg');
+const { Pool, Client } = require('pg');
 
-const dbPool = new Pool({
+const dbPool = new Client({
     host: "ec2-44-192-245-97.compute-1.amazonaws.com",
     user: "ullvmhlbycfuzx",
     port: "5432",
     password: "99fe7c057106d683db7c3fc935684f756f123680db54798f217e5786c11321f9",
     database: "dbvf5314ua3tbq",
-    ssl: true
 });
 
 // const dbPool = new Pool({
