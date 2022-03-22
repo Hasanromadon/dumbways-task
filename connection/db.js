f4a77d199820a3dcbbb2cf6b8684a63832d16283
hasan <hsanromadon@gmail.com>
2022-03-22 10:06:22 +0700

    fix path upload

--- connection/db.js
+++ connection/db.js
@@ -1,21 +1,21 @@
 const { Pool, } = require('pg');
 
-// const dbPool = new Pool({
-//     host: "ec2-34-238-37-113.compute-1.amazonaws.com",
-//     user: "stupvablyvfhuo",
-//     port: "5432",
-//     password: "cc1be3edc71e6781920903cc42a832de9c50cdda9fd652ade1b920fcc3a1378e",
-//     database: "d74494t4318bbn",
-//     ssl: { rejectUnauthorized: false }
-// });
-
 const dbPool = new Pool({
-    host: "localhost",
-    user: "postgres",
-    port: 5432,
-    password: "123",
-    database: "postgres"
+    host: "ec2-34-238-37-113.compute-1.amazonaws.com",
+    user: "stupvablyvfhuo",
+    port: "5432",
+    password: "cc1be3edc71e6781920903cc42a832de9c50cdda9fd652ade1b920fcc3a1378e",
+    database: "d74494t4318bbn",
+    ssl: { rejectUnauthorized: false }
 });
+
+// const dbPool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     port: 5432,
+//     password: "123",
+//     database: "postgres"
+// });
 
 module.exports = dbPool;
 
