be2ea61e81c42a283ac246d51de53989ade1f601
hasan <hsanromadon@gmail.com>
2022-03-22 10:10:27 +0700

    fix path upload

--- middlewares/uploadFile.js
+++ middlewares/uploadFile.js
@@ -2,7 +2,7 @@ const multer = require('multer');
 const path = require('path');
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.join(__dirname, 'uploads', 'projects')); // 
+        cb(null, '/uploads/projects'); // 
     },
     filename: function (req, file, cb) {
         cb(null, Date.now() + '-' + file.originalname.replace(' ', ''));
